c7d48a3992518348e166a37ad9ce52c177335e4c
Lobiten <b.loic38@laposte.net>
2021-08-03 22:19:35 -0400

    New classes coded

--- utils/data_management.py
+++ utils/data_management.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import torch
 import config as cfg
+import tensorflow as tf
 
 
 def create_tensor_from_dataset(dataset: torch.utils.data.Dataset, train: bool=True) -> torch.tensor:
@@ -18,7 +19,7 @@ def create_tensor_from_dataset(dataset: torch.utils.data.Dataset, train: bool=Tr
         img, lbl = dataset[i]
         list_imgs.append(img)
         list_lbls.append(lbl)
-        # TODO : Fix \r trick
+        # FIXME : \r trick
         # print(f"\rStacking {'train' if train else 'test'} : {(i+1) / len(dataset) * 100:.2f}%", end='')
     
     tensor_imgs = torch.stack(list_imgs)
@@ -68,20 +69,33 @@ class TFDataset:
                     raise FileExistsError(f"{file_path} doesn't exist !")
         return data_map
     
-    # FIXME : Remake the dataset with the code written in `tensorflow.ipynb`
-    def get_training_set(self) -> dict:
+    def get_training_set(self) -> tf.data.Dataset:
         """
-        :return: Dictionnary of training set
+        :return: Training dataset
         """
-        return self.dataset['train']
+        images = self.dataset['train']['images']
+        labels = self.dataset['train']['labels']
+        # We reshape images to respect TF format (for convolutions)
+        train_shape = images.shape
+        return tf.data.Dataset.from_tensor_slices((
+            images.reshape((train_shape[0], train_shape[2], train_shape[3], train_shape[1])),
+            labels
+        ))
 
-    def get_test_set(self) -> dict:
+    def get_test_set(self) -> tf.data.Dataset:
         """
-        :return: Dictionnary of testing set
+        :return: Testing dataset
         """
-        return self.dataset['test']
+        images = self.dataset['test']['images']
+        labels = self.dataset['test']['labels']
+        # We reshape images to respect TF format (for convolutions)
+        test_shape = images.shape
+        return tf.data.Dataset.from_tensor_slices((
+            images.reshape((test_shape[0], test_shape[2], test_shape[3], test_shape[1])),
+            labels
+        ))
 
-    def get_image_size(self) -> tuple:
+    def get_original_image_size(self) -> tuple:
         """
         :return: Tuple of image size (C, H, W)
         """
@@ -107,21 +121,45 @@ class TFBatchLoader:
         self.shuffles_nb = shuffles_nb
 
         # Datasets
-        self.cifar = None
-        self.mnist = None
-        self.fashion = None
+        self.cifar = TFDataset('cifar')
+        self.mnist = TFDataset('mnist')
+        self.fashion = TFDataset('fashion')
 
-    def _init_datasets(self):
+        # Mapping (private)
+        self._mapper = {
+            'cifar': self.cifar,
+            'mnist': self.mnist,
+            'fashion': self.fashion
+        }
+    
+    def get_training_loader(self, dataset_name: str, batch_size: int=None, shuffles_nb: int=None) -> tf.data.Dataset:
         """
-        Set the three used datasets
+        Get shuffled and batched training set
+        :param dataset_name: Name of the dataset to return
+        :param batch_size: Batch size
+        :param shuffles_nb: Number of shuffle iterations
+        :return: shuffled and batched set
         """
-        pass
+        if batch_size is None:
+            batch_size = self.batch_size
+        if shuffles_nb is None:
+            shuffles_nb = self.shuffles_nb
+        training_set = self._mapper[dataset_name].get_training_set()
 
-    def _load_dataset(self, name: str):
+        return training_set.shuffle(shuffles_nb).batch(batch_size)
+
+    def get_testing_loader(self, dataset_name: str, batch_size: int=None, shuffles_nb: int=None) -> tf.data.Dataset:
         """
-        Load specified dataset
-        :param name: the name of the dataset ('mnist', 'cifar', 'fashion')
+        Get shuffled and batched testing set
+        :param dataset_name: Name of the dataset to return
+        :param batch_size: Batch size
+        :param shuffles_nb: Number of shuffle iterations
+        :return: shuffled and batched set
         """
-        self.cifar = load_data(cfg.CIFAR_DATA_NAME)
-        self.mnist = load_data(cfg.MNIST_DATA_NAME)
-        self.fashion = load_data(cfg.FASHION_DATA_NAME)
+        if batch_size is None:
+            batch_size = self.batch_size
+        if shuffles_nb is None:
+            shuffles_nb = self.shuffles_nb
+        testing_set = self._mapper[dataset_name].get_testing_set()
+        
+        return testing_set.shuffle(shuffles_nb).batch(batch_size)
